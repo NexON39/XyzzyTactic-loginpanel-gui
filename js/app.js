0463cd901108f9d90d98ed09d4dc42c368df13f7
NexON39 <flashxd30501337@gmail.com>
2022-04-02 20:50:50 +0200

    commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+    const login_box = document.getElementById('login_box');
+    const input1 = document.querySelector('.input_text1');
+    const input2 = document.querySelector('.input_text2');
+
+    window.addEventListener('click', function() {
+        if(input1 == document.activeElement || input2==document.activeElement)
+            login_box.style.border = "1px var(--red) solid";
+        else
+            login_box.style.border = "1px var(--gray-login-border) solid";
+    })
+})
